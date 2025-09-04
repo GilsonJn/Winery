cee7f4b7897b88b2042acf137e024386e24e0cb4
Gilson Dias <gilsondiasjn@gmail.com>
2025-09-04 15:11:10 -0300

    WineOrderClient atualizado
    WineOrderClient atualizado com o wineWarningService

--- Consumer/WineOrderClient/src/main/java/br/com/fiap/winery/ApplicationClient2.java
+++ Consumer/WineOrderClient/src/main/java/br/com/fiap/winery/ApplicationClient2.java
@@ -13,13 +13,25 @@ public class ApplicationClient2 {
         final String TARGET = "http://winery.fiap.com.br/";
         final String NAME = "WineStockServiceImplementationService";
 
+        final String URL2 = "http://localhost:8086/WineWarningService?wsdl";
+        final String TARGET2 = "http://winery.fiap.com.br/";
+        final String NAME2 = "WineWarningServiceImplementationService";
+
         URL url = new URL(URL);
         QName qName = new QName(TARGET, NAME);
         Service service = Service.create(url, qName);
 
+        URL url2 = new URL(URL2);
+        QName qName2 = new QName(TARGET2, NAME2);
+        Service service2 = Service.create(url2, qName2);
+
         WineStockService wineStockService = service.getPort(WineStockService.class);
         String order = wineStockService.placeOrder("João", 2);
         System.out.println(order);
+
+        WineWarningService wineWarningService = service2.getPort(WineWarningService.class);
+        String warn = wineWarningService.sendWarn();
+        System.out.println(warn);
 
 
     }
