2afd1ad2f5adc9b4841faf7417b660711313e9dc
Gilson Dias <gilsondiasjn@gmail.com>
2025-09-04 15:02:59 -0300

    WineWarningService funcionando
    SOAP WineWarningService funcionando

--- Publisher/WinerySys/src/main/java/br/com/fiap/winery/Loader.java
+++ Publisher/WinerySys/src/main/java/br/com/fiap/winery/Loader.java
@@ -5,9 +5,12 @@ import jakarta.xml.ws.Endpoint;
 public class Loader {
     public static void main(String[] args) {
         WineStockServiceImplementation wineStock = new WineStockServiceImplementation();
+        WineWarningServiceImplementation wineWarning = new WineWarningServiceImplementation();
 
         Endpoint.publish("http://localhost:8085/WineStockService", wineStock);
         System.out.println("Serviço publicado!");
+        Endpoint.publish("http://localhost:8086/WineWarningService", wineWarning);
+        System.out.println("Serviço Warning publicado!");
 
     }
 }
